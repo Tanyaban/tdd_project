31acbc0f0dd015f394d936b3ebe2b9988e43f699
Tanya Bansal <tani.wilsonia@gmail.com>
2018-08-24 09:15:09 +0530

    commit

--- TDD/src/tdd/Test_Runner.java
+++ TDD/src/tdd/Test_Runner.java
@@ -21,7 +21,7 @@ public class Test_Runner {
 		  System.setProperty("webdriver.gecko.driver","/usr/local/bin/");
 		  driver=new FirefoxDriver();
 		  
-		  driver.get("http://13.251.143.66:8090//IPL-Fantasy-League-0.0.1-SNAPSHOT/");
+		  driver.get("http://13.251.143.66:8090/IPL-Fantasy-League-0.0.1-SNAPSHOT/");
 		  //sThread.sleep(5000);
 	  }
 
